746f82a3ea3ff6900ebd7b9c3d95c0fa28325862
Aman David <amandavid0032@gmail.com>
2024-01-24 17:30:02 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include'main.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="#" enctype="multipart/form-data">
+        First Name <input name="firstname" type="text">
+        Last Name <input name="lastname" type="text">
+        Age <input name="age" type="number">
+        Email ID <input name="email" type="text">
+        Phone No <input name="phoneno" type="number">
+        Gender:
+        <label><input type="radio" name="gender" value="male"> Male</label>
+        <label><input type="radio" name="gender" value="female"> Female</label>
+        Image <input type="file" name="image">
+        <button name="submit" value="submit" type="submit">Submit</button>
+    </form>
+</body>
+</html>
